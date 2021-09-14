ba1a357ccf5383e70fa08d62b53be801b71bc948
dani01S <dany_mccalister@yahoo.com>
2021-09-14 11:52:57 +0300

    Implemented register request and handle request

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -4,13 +4,19 @@ import "../Styles/login.css";
 import {Link} from 'react-router-dom';
 import "../Styles/SignUp.css";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 let firstPasField ='';
 let repeatPassField = '';
 let emVl = false;
 
+let oldUsername = null;
+let oldEmail = null;
+
+
 const SignUp  = () =>{
 
+let history = useHistory();
 const validateEmail = (email)=>{
   var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(email)) {
@@ -27,30 +33,46 @@ let [validForm,setValidForm] = useState(false);
 let [password,setPassword] = useState("");
 let [passwordMatch,setPasswordMatch] = useState(false);
 let [usernameExist,setUserNameExist] = useState(false);
+let [emailExists,setEmailExists] = useState(false);
 let [username,setUsername] = useState("");
 let [repeatPass,setRepeatPass] = useState("");
 
-
 useEffect(()=>{
-  if(passwordMatch && validEmail && username!='' && emailVal!='' && password!='')
+  if(passwordMatch && validEmail && username!='' && emailVal!='' && password!='' && !emailExists && !usernameExist)
   {
     setValidForm(true);
   } else {
     setValidForm(false);
   }
 
-},[username,emailVal,password,repeatPass,passwordMatch,validEmail]);
+},[username,emailVal,password,repeatPass,passwordMatch,validEmail,emailExists,usernameExist]);
 
 const handleUserName = (txt)=>{
   const val = txt.target.value;
+  if(oldUsername !=null)
+  {
+    if(val!=oldUsername)
+    {
+      setUserNameExist(false);
+    }
+  }
   setUsername(val);
 }
 
 const handleEmail = (txt) =>{
   const value = txt.target.value;
+
   if(validateEmail(value))
   {
     setValidEmail(true);
+    if(oldEmail!=null)
+    {
+      if(value!=oldEmail)
+      {
+        setEmailExists(false);
+      }
+    }
+
   } else {
     setValidEmail(false);
   }
@@ -81,7 +103,8 @@ const handleRepeatPassword = (txt)=>{
 
 
 const handleReq = async (e)=>{
-
+  oldUsername = null;
+  oldEmail = null;
   const reqBody = {
     email:emailVal,
     password:password,
@@ -94,7 +117,34 @@ const handleReq = async (e)=>{
     method:'post',
     url:"http://localhost:8000/api/auth/register",
     data:reqBody
+  }).then(resp=>{
+      console.log(resp);
+      if(resp.status == 200)
+      {
+        history.push("/register-success");
+      }
+  }).catch(err=>{
+    console.log(err.response);
+    if(err.response.data == "Username and email already exists!")
+    {
+          oldUsername = username;
+          oldEmail = emailVal;
+          setUserNameExist(true);
+          setEmailExists(true);
+    }
+    else if(err.response.data == "Username already exists!")
+    {
+          oldUsername = username;
+          setUserNameExist(true);
+    }
+    else if(err.response.data == "Email already exists!")
+    {
+        oldEmail = emailVal;
+        setEmailExists(true);
+    }
+
   });
+
 
 }
 
@@ -107,12 +157,12 @@ return (
            Sign Up
          </span>
 
-         <div className="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
+         <div className="wrap-input100 validate-input m-b-23" data-validate = "Username is required">
            <span className="label-input100">Username</span>
            <input className="input100" type="text" value={username} onChange={handleUserName} name="username" placeholder="Type your username"/>
            <span className="focus-input100" data-symbol="&#xf206;"></span>
          </div>
-         <p className="wrong-credentials not-display">Username taken!</p>
+         <p className={usernameExist?"wrong-credentials":"wrong-credentials not-display"}>Username taken!</p>
        <div className="wrap-input100 validate-input m-b-23" style={{"marginTop":'30px'}} data-validate = "Username is reauired">
            <span className="label-input100">Email</span>
            <input className="input100" type="text" onChange={handleEmail} name="username" value={emailVal} placeholder="Type your email"/>
@@ -120,7 +170,7 @@ return (
          </div>
 
          <p className={validEmail?"wrong-credentials not-display":"wrong-credentials"}>Email not valid!!!</p>
-         <br/><span className="wrong-credentials not-display">There is already an account registerd with this email!</span>
+         <br/><span className={emailExists?"wrong-credentials":"not-display"}>There is already an account registerd with this email!</span>
          <div className="wrap-input100 validate-input" data-validate="Password is required">
            <span className="label-input100">Password</span>
            <input className="input100" type="password" value={password} onChange={handlePassword} name="pass" placeholder="Type your password"/>
