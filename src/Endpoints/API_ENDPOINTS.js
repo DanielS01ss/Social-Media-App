27b1a4bad2a7aa428cbbe87613f3c7a24178b2d5
dani01S <dany_mccalister@yahoo.com>
2021-09-20 17:27:08 +0300

    Prepared refresh Token

--- src/Endpoints/API_ENDPOINTS.js
+++ src/Endpoints/API_ENDPOINTS.js
@@ -2,5 +2,7 @@ const LOGIN_URL = 'http://localhost:8000/api/auth/login';
 const SIGNUP_URL = 'http://localhost:8000/api/auth/register';
 const DELETE_TOKEN_URL = "http://localhost:8000/api/auth/logout";
 const CHECK_TOKEN_URL = "http://localhost:8000/api/auth/checkToken";
+const GET_USER_URL = "http://localhost:8000/api/auth/user";
+const UPDATE_USER = (userId)=>{ return `http://localhost:8000/api/user/${userId}/update`}
 
-export {LOGIN_URL,SIGNUP_URL,DELETE_TOKEN_URL,CHECK_TOKEN_URL};
+export {LOGIN_URL,SIGNUP_URL,DELETE_TOKEN_URL,CHECK_TOKEN_URL,GET_USER_URL,UPDATE_USER};
